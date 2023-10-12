5b89ce8fdf8b9a4dc4686cb4aef5d9445adfd506
Ajay_Bains <ajay.bains@codvo.ai>
2023-10-13 04:52:56 +0530

    conneted to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("connected to db");
+  })
+  .catch((err) => console.log("err", err));
 const app = express();
 app.listen(3000, () => {
   console.log("server is listening on port 3000!!");
